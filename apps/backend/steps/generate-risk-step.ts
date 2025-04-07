c112e8cc7c748dd9257916b478a09fe5068712e4
bjasz <bjasz@diligent.com>
2025-04-07 14:45:12 +0200

    feat: split input step v2

--- apps/backend/steps/generate-risk-step.ts
+++ apps/backend/steps/generate-risk-step.ts
@@ -1,7 +1,16 @@
 import {callBedrock} from "../ai-client";
 
+type Input = {
+    results: string[]
+}
+
 export const generateRiskStep = async (input: string): Promise<string> => {
-    const prompt = `Generate risk from the given text. The response should be in JSON format. It should have a title, description and a category. Available categories: Operational, IT/Infosec, Financial, Regulatory/Complience. Input text: ${input}`;
-    const response = await callBedrock(prompt);
-    return response.content[0].text;
+    const data: Input = JSON.parse(input);
+    const prompts = data.results.map(async paragraph => {
+        const prompt = `Generate risk from the given text. The response should be in JSON format. It should have a title, description and a category. Available categories: Operational, IT/Infosec, Financial, Regulatory/Complience. Input text: ${paragraph}`
+        const response = await callBedrock(prompt);
+        return response.content[0].text;
+    });
+    const responses = await Promise.all(prompts);
+    return JSON.stringify({results: responses});
 };
