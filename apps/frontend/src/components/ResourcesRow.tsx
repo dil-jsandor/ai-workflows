5d3c90883f38a4abde389dd7252a3504792b2ed3
Martin Lényi <mlenyi@diligent.com>
2025-04-08 10:14:24 +0200

    feat: style main menu icons

--- apps/frontend/src/components/ResourcesRow.tsx
+++ apps/frontend/src/components/ResourcesRow.tsx
@@ -1,19 +1,20 @@
 export default function ResourcesRow() {
   return (
-    <div
-      id="risk-and-strategy-row"
-      className="w-full px-[40px] flex flex-col gap-y-[28px]"
-    >
-      <span className="text-[26px] text font-semibold">Resources</span>
-      <div className="w-[189px] h-[140px] border-gray-200 border rounded-md bg-white shrink-0">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 40 40">
-          <path
-            d="M7.92766 12.4847V32.0722H32.0727V12.4847H25.8335V25.2484L20.0002 22.3318L14.1668 25.2484V12.4847H7.92766ZM5.8335 34.1663V10.5788L9.56016 5.83301H30.4072L34.1668 10.6109V34.1663H5.8335ZM8.59516 10.3909H31.3614L29.3493 7.92718H10.6177L8.59516 10.3909ZM16.261 12.4847V21.8488L20.0002 19.9793L23.7393 21.8488V12.4847H16.261Z"
-            fill="currentColor"
-          ></path>
-        </svg>
-        <span className="font-semibold text-[16px]">Installers</span>
+      <div
+          id="risk-and-strategy-row"
+          className="w-full px-[40px] flex flex-col gap-y-[28px]"
+      >
+          <span className="text-[26px] text font-semibold">Resources</span>
+          <div
+              className="w-[189px] h-[140px] border-gray-200 border rounded-md bg-white shrink-0 flex flex-col justify-center items-center cursor-pointer hover:bg-gray-100">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 40 40" className="h-[40px] w-[40px] text-red-700">
+                  <path
+                      d="M7.92766 12.4847V32.0722H32.0727V12.4847H25.8335V25.2484L20.0002 22.3318L14.1668 25.2484V12.4847H7.92766ZM5.8335 34.1663V10.5788L9.56016 5.83301H30.4072L34.1668 10.6109V34.1663H5.8335ZM8.59516 10.3909H31.3614L29.3493 7.92718H10.6177L8.59516 10.3909ZM16.261 12.4847V21.8488L20.0002 19.9793L23.7393 21.8488V12.4847H16.261Z"
+                      fill="currentColor"
+                  ></path>
+              </svg>
+              <span className="font-semibold text-[16px]">Installers</span>
+          </div>
       </div>
-    </div>
   );
 }
--- apps/frontend/src/components/RiskAndStrategyRow.tsx
+++ apps/frontend/src/components/RiskAndStrategyRow.tsx
@@ -8,8 +8,8 @@ export default function RiskAndStrategyRow() {
         Risk & <span className="text-red-700">Strategy</span>
       </span>
       <div className="gap-[16px] flex w-full flex-wrap">
-        <div className="w-[189px] h-[140px] border-gray-200 border rounded-md bg-white shrink-0">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 40 40">
+        <div className="w-[189px] h-[140px] border-gray-200 border rounded-md bg-white shrink-0 flex flex-col justify-center items-center cursor-pointer hover:bg-gray-100">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 40 40" className="h-[40px] w-[40px] text-red-700">
             <path
               d="M18.2051 25.1539L27.0897 16.28L25.6175 14.8142L18.267 22.1647L14.391 18.2886L12.8739 19.8057L18.2051 25.1539ZM20 35.7842C16.3839 34.7942 13.3965 32.65 11.0379 29.3516C8.67931 26.0532 7.5 22.389 7.5 18.3591V8.89333L20 4.21387L32.4999 8.89333V18.3591C32.4999 22.389 31.3206 26.0532 28.962 29.3516C26.6034 32.65 23.616 34.7942 20 35.7842ZM20 33.5899C23.0356 32.6091 25.5288 30.6995 27.4797 27.8612C29.4305 25.0229 30.406 21.8555 30.406 18.3591V10.3431L20 6.44028L9.59396 10.3431V18.3591C9.59396 21.8555 10.5694 25.0229 12.5202 27.8612C14.4711 30.6995 16.9643 32.6091 20 33.5899Z"
               fill="currentColor"
@@ -17,8 +17,8 @@ export default function RiskAndStrategyRow() {
           </svg>
           <span className="font-semibold text-[16px]">Risk Manager</span>
         </div>
-        <div className="w-[189px] h-[140px] border-gray-200 border rounded-md bg-white shrink-0">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 40 40">
+        <div className="w-[189px] h-[140px] border-gray-200 border rounded-md bg-white shrink-0 flex flex-col justify-center items-center cursor-pointer hover:bg-gray-100">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 40 40" className="h-[40px] w-[40px] text-red-700">
             <path
               d="M16.0662 24.8227L20 21.8633L23.8333 24.7907L22.2905 20.0642L26.2883 17.0835H21.485L20 12.3527L18.5149 17.0835H13.7116L17.626 20.0642L16.0662 24.8227ZM20 35.7842C16.3839 34.7942 13.3965 32.65 11.0379 29.3516C8.67931 26.0532 7.5 22.389 7.5 18.3591V8.89333L20 4.21387L32.4999 8.89333V18.3591C32.4999 22.389 31.3206 26.0532 28.962 29.3516C26.6034 32.65 23.616 34.7942 20 35.7842ZM20 33.5899C23.0356 32.6091 25.5288 30.6995 27.4797 27.8612C29.4305 25.0229 30.406 21.8555 30.406 18.3591V10.3431L20 6.44028L9.59396 10.3431V18.3591C9.59396 21.8555 10.5694 25.0229 12.5202 27.8612C14.4711 30.6995 16.9643 32.6091 20 33.5899Z"
               fill="currentColor"
@@ -26,8 +26,8 @@ export default function RiskAndStrategyRow() {
           </svg>
           <span className="font-semibold text-[16px]">Strategy</span>
         </div>
-        <div className="w-[189px] h-[140px] border-gray-200 border rounded-md bg-white shrink-0">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 40 40">
+        <div className="w-[189px] h-[140px] border-gray-200 border rounded-md bg-white shrink-0 flex flex-col justify-center items-center cursor-pointer hover:bg-gray-100">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 40 40" className="h-[40px] w-[40px] text-red-700">
             <path
               fill-rule="evenodd"
               clip-rule="evenodd"
